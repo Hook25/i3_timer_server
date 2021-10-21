6bd3bf7946245d14ecf8d89cb9e061762e8e89f4
h25 <>
2021-10-21 13:03:23 +0200

    Init repo

---
+++ client.py
@@ -0,0 +1,103 @@
+import requests
+import argparse
+import json
+from datetime import datetime
+
+def poss_act(x):
+  assert x in ["show_brief", "show_detail", "post_new", "delete"]
+  return x
+
+def load_config():
+  parser = argparse.ArgumentParser(description="Queries the remote timer server")
+  parser.add_argument('-ip', default="127.0.0.1", help="IP of the remote server")
+  parser.add_argument('-port', default=7853, help="Port of the remote server")
+  parser.add_argument('-action', default='show_brief', type=poss_act, 
+    help="Possible actions out of post_new, delete, show_brief, show_detail"
+  ) 
+  parser.add_argument('additional', metavar='id or spec', nargs="*", 
+    help="Either id when action delete or time (s or m:s or h:m:s or d:h:m:s) when action post_new"
+  )
+  return parser.parse_args()
+
+def update_active(conf):
+  req = requests.get("http://{}:{}/timers".format(conf.ip, conf.port))
+  return req.json()
+
+def _delete_timer(conf, i):
+  h = {"Content-Type" : "application/json"}
+  req = requests.delete(
+    "http://{}:{}/timers".format(conf.ip, conf.port),
+    data = json.dumps({"id" : i}),
+    headers=h
+  )
+  return req.status_code
+
+def delete_timer(conf):
+   res = [_delete_timer(conf, i) for i in conf.additional]
+   if all(x == 200 for x in res):
+     print("Success")
+   else:
+     print("Failed to delete:", ", ".join(i for (i, r) in zip(conf.additional, res) if res != 200)) 
+
+def to_t(s):
+  s = list(reversed(s.split(":")))
+  mult = [1, 60, 60**2, 60**2*24]
+  t = zip(mult[:len(s)], (float(k) for k in s))
+  time_s = sum(a*b for (a,b) in t)
+  return time_s
+
+def _create_timer(conf, t):
+  h = {"Content-Type" : "application/json"}
+  req = requests.post(
+    "http://{}:{}/timers".format(conf.ip, conf.port),
+    headers = h,
+    data = json.dumps({"seconds" : t, "message" : conf.additional[-1]})
+  )
+  if req.status_code == 200:
+    print("Success")
+  else:
+    print("Failed with code: %s" % req.status_code)
+
+def create_timer(conf):
+  if len(conf.additional) != 2:
+    print("Provide 2 additional arguments: time message")
+  else:
+    _create_timer(conf, to_t(conf.additional[0]))
+
+def get_time_diff(a, b):
+  a = datetime.strptime(a, "%m/%d/%Y%H:%M:%S")
+  b = datetime.strptime(b, "%m/%d/%Y%H:%M:%S")
+  return (b - a)
+
+def pretty_print_short(req):
+  to_print = [
+    "[{}]".format(get_time_diff(v['time_now'], v['end_time'])) for v in req.values()
+  ]
+  print("".join(to_print))
+
+def pretty_print_long(req):
+  to_print = [
+    "[{}] ({} -> {}) [{}]".format(
+      v['id'], 
+      v['start_time'],
+      v['end_time'],
+      get_time_diff(v['time_now'], v['end_time'])
+    ) for v in req.values()
+  ]
+  print("\n".join(to_print))
+
+def main():
+  conf = load_config()
+  if conf.action == 'show_brief':
+    active = update_active(conf)
+    pretty_print_short(active)
+  elif conf.action == 'show_detail':
+    active = update_active(conf)
+    pretty_print_long(active)
+  elif conf.action == 'delete':
+    delete_timer(conf)
+  elif conf.action == 'post_new':
+    create_timer(conf)
+
+if __name__ == "__main__":
+  main()
---
+++ server.py
@@ -0,0 +1,129 @@
+from enum import Enum
+from flask import Flask, request, abort
+from threading import Lock
+from copy import deepcopy
+from datetime import datetime, timedelta
+from multiprocessing import Process, Event
+from os import system
+import hashlib
+
+import json
+
+api = Flask(__name__)
+
+started_timers = {}
+started_timers_lock = Lock()
+
+config = None #gets set by load_config
+
+def get_timers(request):
+  """
+  Cleans up the list and encodes it into json
+  """
+  time_now = datetime.now().strftime("%m/%d/%Y%H:%M:%S")
+  started_timers_lock.acquire()
+  to_remove = [k for (k, ts) in started_timers.items() if not ts.process.is_alive()]
+  for tr in to_remove:
+    del started_timers[tr]
+  to_send = { 
+    i : {
+      "id" : v.get_id(),
+      "start_time" : v.start_time,
+      "time_now" : time_now,
+      "end_time" : v.end_time,
+      "message" : v.message 
+    } for (i, v) in started_timers.items()
+  }
+  started_timers_lock.release()
+  return json.dumps(to_send) 
+
+def post_timers(request):
+  """
+  Tries to create a timer with the given spec starting now
+  """
+  err = False
+  to_ins = request.json
+  ts = TimerSpec(to_ins["seconds"], to_ins["message"], config.command)
+  ts_id = ts.get_id()
+  started_timers_lock.acquire()
+  if ts_id not in started_timers:
+    started_timers[ts_id] = ts
+  else:
+    err = True
+  started_timers_lock.release()
+  ts.start_spec()
+  if err:
+    abort(404)
+  return "200"
+
+def del_timers(request):
+  err = False
+  key = request.json["id"]
+  started_timers_lock.acquire()
+  try:
+    started_timers[key].process.kill()
+    del started_timers[key]
+  except KeyError:
+    err = True
+  started_timers_lock.release()
+  if err:
+    abort(404)
+  return to_r
+
+@api.route('/timers', methods=['GET', 'POST', 'DELETE'])
+def timers():
+  if request.method == 'GET':
+    return get_timers(request)
+  elif request.method == 'POST':
+    return post_timers(request)
+  elif request.method == 'DELETE':
+    return del_timers(request)
+
+class TimerSpec:
+  def __init__(self, seconds, msg, cmd):
+    self.message = msg
+    self.cmd = cmd
+    now = datetime.now()
+    self.start_time = now.strftime("%m/%d/%Y%H:%M:%S")
+    self.end_time = (now + timedelta(seconds=float(seconds))).strftime(
+      "%m/%d/%Y%H:%M:%S"
+    )
+    self.time_span = seconds
+  def get_id(self):
+    m = hashlib.sha256()
+    m.update("{}{}{}".format(self.message, self.start_time, self.time_span).encode())
+    return m.hexdigest()
+  def _runner(self, t, msg, cmd):
+    e = Event()
+    e.wait(t)
+    system(cmd.format(msg))
+  def start_spec(self):
+    p = Process(
+      target = self._runner, args=(float(self.time_span), self.message, self.cmd)
+    )
+    p.start()
+    self.process = p
+    self.pid = p.pid
+
+def load_config():
+  global config
+  import argparse
+  parser = argparse.ArgumentParser(description="Queries the remote timer server")
+  parser.add_argument('-ip', default="127.0.0.1", help="IP of the remote server")
+  parser.add_argument('-port', default=7853, help="Port of the remote server")
+  parser.add_argument(
+    '-command', default='zenity --info --title="ExpiredTimer" --text="{}"',
+    help="Command to run on timer expired"
+  )
+  config = parser.parse_args()
+  return {"port" : int(config.port), "ip" : config.ip}
+
+def start_server(config):
+  api.run(host = config["ip"], port = config["port"], threaded = True)
+
+def main():
+  config = load_config()
+  start_server(config)
+
+if __name__ == "__main__":
+  main()
